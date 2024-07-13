c98c74a7c85ffbfa2850a5959bac0ab291f33139
delgadoarlee <delgadoarlee@gmail.com>
2024-07-13 18:02:26 +0800

    Add seed

--- database/mongodb/index.ts
+++ database/mongodb/index.ts
@@ -3,6 +3,8 @@ import { MongoClient } from 'mongodb';
 let client: MongoClient;
 
 const mongoDbClient = () => {
+    console.log(process.env.MONGODB_CONNECTION_URI);
+    console.log(process.env.path);
     if (!client) {
         return (client = new MongoClient(process.env.MONGODB_CONNECTION_URI!));
     }
---
+++ database/mongodb/seed.ts
@@ -0,0 +1,137 @@
+import 'dotenv/config';
+import { ObjectId } from 'mongodb';
+import { faker } from '@faker-js/faker';
+import mongoDbClient from './index';
+
+const chats = [
+    {
+        _id: new ObjectId(),
+        user: {
+            id: new ObjectId(),
+            clerkId: 'user1_clerk_id',
+            email: 'user1@example.com',
+            profilePicture: 'user1.jpg',
+            lastName: 'Doe',
+            firstName: 'John',
+            contactNumber: '1234567890',
+            gender: 'Male',
+            events: [],
+            chats: [],
+            vendor: null,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        vendor: {
+            id: new ObjectId(),
+            clerkId: 'vendor1_clerk_id',
+            email: 'vendor1@example.com',
+            profilePicture: 'vendor1.jpg',
+            name: 'Vendor One',
+            address: '123 Vendor St, Vendorville',
+            contactNumber: '0987654321',
+            bio: 'Lorem ipsum dolor sit amet.',
+            logo: 'vendor1_logo.jpg',
+            tags: [],
+            visibility: true,
+            credibiltyfactor: 0.9,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        messages: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+];
+
+const createMessageData = (senderId: string) => {
+    const date = faker.date.anytime();
+
+    return {
+        _id: new ObjectId(),
+        senderId: new ObjectId(senderId),
+        content: faker.lorem.sentence({ min: 3, max: 20 }),
+        timestamp: date,
+        createdAt: date,
+        updatedAt: date,
+    };
+};
+
+const getRandomId = (userId: string, vendorId: string) => {
+    const randomIndex = Math.random() < 0.5 ? 0 : 1;
+    return randomIndex === 0 ? userId : vendorId;
+};
+
+const createManyMessageData = (
+    userId: string,
+    vendorId: string,
+    count: number
+) => {
+    const senderId = getRandomId(userId, vendorId);
+
+    return Array.from(Array(count), () => createMessageData(senderId));
+};
+
+const createChatData = (ids: { userId: string; vendorId: string }) => {
+    const { userId, vendorId } = ids;
+
+    return {
+        _id: new ObjectId(),
+        user: new ObjectId(userId),
+        vendor: new ObjectId(vendorId),
+        messages: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    };
+};
+
+const client = mongoDbClient();
+
+// Function to seed data into MongoDB
+async function seedData() {
+    mongoDbClient()
+        .on('serverOpening', () => console.log('DB Connected: Seed Start'))
+        .on('serverClosed', () => console.log('DB Disconnected: Seed Done'))
+        .on('error', (error) =>
+            console.log('An Error has Occured while Seeding:', error)
+        )
+        .connect();
+
+    const userId = '667cb0663f424f61b551892a';
+    const vendorId = '667ff5df7cfc46333461187b';
+
+    try {
+        const database = client.db();
+        const chatsCollection = database.collection('chats');
+        const messagesCollection = database.collection('messages');
+        const chatData = createChatData({ userId, vendorId });
+        const messagesData = createManyMessageData(userId, vendorId, 10);
+
+        const insertChatResult = await chatsCollection.insertOne(chatData);
+        console.log(`Inserted chat with ID: ${insertChatResult.insertedId}`);
+
+        const chatId = insertChatResult.insertedId;
+        chatData._id = chatId;
+
+        // Insert messages and update chat with message IDs
+        const messageIds: ObjectId[] = [];
+        for (const messageData of messagesData) {
+            const insertMessageResult =
+                await messagesCollection.insertOne(messageData);
+            const messageId = insertMessageResult.insertedId;
+            messageIds.push(messageId);
+        }
+
+        // Update chat document with message IDs
+        await chatsCollection.updateOne(
+            { _id: chatId },
+            { $set: { messages: messageIds } }
+        );
+    } catch (err) {
+        console.error('Error seeding data:', err);
+    } finally {
+        await client.close();
+    }
+}
+
+// Call the seedData function to execute seeding
+seedData();
